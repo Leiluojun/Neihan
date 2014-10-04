536a22eb1b7b415cf512a8f0c7a7ec95c184af9b
Yuhj <lin5667181@163.com>
2014-10-04 19:02:39 +0800

    frr

--- Neihan/src/com/yuhj/neihanduanzi/text/TextActivity.java
+++ Neihan/src/com/yuhj/neihanduanzi/text/TextActivity.java
@@ -1,17 +1,21 @@
 package com.yuhj.neihanduanzi.text;
 
+import java.util.ArrayList;
+
 import m.framework.network.ResponseCallback;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.yuhj.neihanduanzi.R;
+import com.yuhj.neihanduanzi.bean.ImageEntity;
+import com.yuhj.neihanduanzi.bean.ImageList;
+import com.yuhj.neihanduanzi.client.ClientAPI;
+
 import android.os.Bundle;
 import android.R.integer;
 import android.app.Activity;
@@ -27,64 +31,29 @@ import android.view.Menu;
  * @version 1.0
  */
 
-public class TextActivity extends Activity implements Response.Listener<String>{
+public class TextActivity extends Activity implements Response.Listener<String> {
 	/**
 	 * 分类ID,代表文本
 	 */
-	private final static int CATEGORY_ARTICLE = 1;
+	public final static int CATEGORY_ARTICLE = 1;
 	/**
 	 * 分类ID,代表图片
 	 */
 	private final static int CATEGORY_PICTURE = 2;
 
+	/**
+	 * 用于请求网络的队列
+	 */
+	private RequestQueue queue;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_text);
+		queue = Volley.newRequestQueue(this);
 		// 每次获取的文章的个数
 		int itemCount = 30;
-		getList(CATEGORY_ARTICLE, itemCount,this);
-	}
-
-	/**
-	 * 获取内涵段子的列表内容
-	 * 
-	 * @param CateGroyType
-	 *            要获取的参数类型
-	 * @param itemCount
-	 *            每次要获取的条目数
-	 * @see #CATEGORY_ARTICLE
-	 * @see #CATEGORY_PIVTURE
-	 */
-	private void getList(int CateGroyType, int itemCount,
-			Response.Listener<String> responseListener) {
-		RequestQueue queue = Volley.newRequestQueue(this);
-		String CATEGORY_LIST_API = "http://ic.snssdk.com/2/essay/zone/category/data/";
-		// 分类参数,根据类型获取不同的数据
-		String categoryParam = "category_id=" + CateGroyType;
-		String countParam = "count=" + itemCount;
-		String device_type = "device_type=KFTT";
-		String openUDID = "openudid=b90ca6a3a19a78d6";
-		String url = CATEGORY_LIST_API
-				+ "?"
-				+ categoryParam
-				+ "&"
-				+ countParam
-				+ "&"
-				+ device_type
-				+ "&"
-				+ openUDID
-				+ "&level=6&iid=2337593504&device_id=2757969807&ac=wifi&channel=wandoujia&aid=7&app_name=joke_essay&version_code=302&device_platform=android&os_api=15&os_version=4.0.3";
-		queue.add(new StringRequest(Request.Method.GET, url, responseListener,
-				new Response.ErrorListener() {
-
-					@Override
-					public void onErrorResponse(VolleyError arg0) {
-						// TODO Auto-generated method stub
-
-					}
-				}));
-
+		ClientAPI.getList(queue, CATEGORY_PICTURE, itemCount, this);
 	}
 
 	@Override
@@ -98,12 +67,23 @@ public class TextActivity extends Activity implements Response.Listener<String>{
 	public void onResponse(String result) {
 		try {
 			JSONObject json = new JSONObject(result);
-			System.out.println("------->" + json.toString());
+			// System.out.println("------->" + json.toString());
+			// 获得根目录下的Data对象
+			JSONObject object = json.getJSONObject("data");
+			// 从data对象中，获取名称为data的数组
+			JSONArray array = object.getJSONArray("data");
+			int len = array.length();
+			if (len > 0) {
+				// 遍历数组中每一条段子的信息
+				for (int i = 0; i < len; i++) {
+					JSONObject item = array.getJSONObject(i);
+					ImageEntity imageEntity =new ImageEntity();
+					imageEntity.ParseImageItem(item);
+				}
+			}
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 	}
-
-
 
 }
